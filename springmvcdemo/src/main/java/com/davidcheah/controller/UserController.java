a2e92afbac486cd4f6c8f8a62067077bb09d879a
DavidCheah <tattwei46@gmail.com>
2019-07-14 22:44:57 +0800

    Update REST methods to include responseentity

--- springmvcdemo/src/main/java/com/davidcheah/controller/UserController.java
+++ springmvcdemo/src/main/java/com/davidcheah/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.davidcheah.model.User;
@@ -24,15 +23,22 @@ public class UserController {
 
 	// http://localhost:8080/users
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
-	public List<User> getAllUsers() {
-		List<User> users = service.getAllUsers();
-		return users;
+	public ResponseEntity<List<User>> getAllUsers() {
+		List<User> foundUsers = service.getAllUsers();
+		if (foundUsers.size() > 0) {
+			return ResponseEntity.ok(foundUsers);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 	}
 
 	// http://localhost:8080/users/1
 	@RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
-	public User getUser(@PathVariable int id) {
-		return service.getUser(id);
+	public ResponseEntity<User> getUser(@PathVariable int id) {
+		User foundUser = service.getUser(id);
+		if (foundUser != null) {
+			return ResponseEntity.ok(foundUser);
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 	}
 
 	// http://localhost:8080/users
@@ -40,17 +46,17 @@ public class UserController {
 	public ResponseEntity<String> createUser(@RequestBody User user) {
 		User newUser = service.add(user);
 		if (newUser != null) {
-			return new ResponseEntity<String>("Added user id: " + newUser.getId(), HttpStatus.OK);
-		} return new ResponseEntity<String>("User not found", HttpStatus.NOT_IMPLEMENTED);
+			return ResponseEntity.ok().body("Added user id: " + newUser.getId());
+		} return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 	}
 
-	// http://localhost:8080/users
+	// http://localhost:8080/users/1
 	@RequestMapping(value = "/users/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<String> updateUser(@PathVariable int id, @RequestBody User user) {
 		User updatedUser = service.update(id, user);
 		if (updatedUser != null) {
-			return new ResponseEntity<String>("Updated user id: " + updatedUser.getId(), HttpStatus.OK);
-		} return new ResponseEntity<String>("User not found", HttpStatus.NOT_IMPLEMENTED);
+			return ResponseEntity.ok().body("Updated user id: " + updatedUser.getId());
+		} return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
 	}
 
 	// http://localhost:8080/users/1
@@ -58,7 +64,7 @@ public class UserController {
 	public ResponseEntity<String> deleteUser(@PathVariable int id) {
 		User removedUser = service.delete(id);
 		if (removedUser != null) {
-			return new ResponseEntity<String>("Removed user id: " + removedUser.getId(), HttpStatus.OK);
-		} return new ResponseEntity<String>("User not found", HttpStatus.NOT_IMPLEMENTED);
+			return ResponseEntity.ok().body("Removed user id: " + removedUser.getId());
+		} return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
 	}
 }
